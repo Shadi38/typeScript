abdfd3a349025c0f72f230802cd43ae8f8073e4e
shadi fakhri <113238379+Shadi38@users.noreply.github.com>
2025-03-06 22:39:47 +0000

    added changes in Header component

--- contex API & useReducer/src/components/Header.tsx
+++ contex API & useReducer/src/components/Header.tsx
@@ -1,11 +1,16 @@
 import Button from './UI/Button.tsx';
+// import { useContext } from 'react';
+import {  useTimersContext } from '../store/timers-contex.tsx';
 
 export default function Header() {
+  //we can have access to the context value(object) by using the useContext hook
+  // const timersCtx = useContext(TimersContext);
+  const timersCtx = useTimersContext();//with this approch we can avoid the above line and we TimerContext object never be null
   return (
     <header>
       <h1>ReactTimer</h1>
 
-      <Button>Stop Timers</Button>
+      <Button>{timersCtx.isRunning?'Stop':'Start'} Timers</Button>
     </header>
   );
 }
--- contex API & useReducer/src/store/timers-contex.tsx
+++ contex API & useReducer/src/store/timers-contex.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import {type ReactNode, createContext, useContext } from "react";
 
 type Timer = {
     name: string;
@@ -15,5 +15,29 @@ type TimersContextValue = TimersState & {
     startTimers:()=>void;
     stopTimers:()=>void;
 }
-//create contex object which we will need for managing that context
-const TimersContext = createContext<TimersContextValue | null>(null)
+//create contex object which we will need for managing that context across the application
+ const TimersContext = createContext<TimersContextValue | null>(null)
+//we create custom hook to use the context,we can just call hook function in component to get the context value
+export function useTimersContext() {
+   const TimerCtx = useContext(TimersContext)
+   if (!TimerCtx) {
+       throw new Error('TimersContext is null- that should not be the case')
+   }
+   return TimerCtx
+}
+
+type TimersContextProviderProps = {
+    children: ReactNode;
+}
+export default function TimersContextProvider({children}:TimersContextProviderProps){
+    const ctx : TimersContextValue = {
+        isRunning: false,
+        timers: [],
+        addTimer: (timerData:Timer)=>{},
+        startTimers: ()=>{},
+        stopTimers: ()=>{}
+    }
+    return(
+    <TimersContext.Provider value={ctx}>{children}</TimersContext.Provider>
+    )
+ }
